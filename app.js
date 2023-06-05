9bc1e1ed92f3b61e2565246b2202f2c9baed915e
Chetan Sengar <nathan4drake1999@gmail.com>
2023-06-05 13:39:59 +0530

    added database config and initialzation

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
+const connectDB = require("./db/connect");
 const express = require("express");
 
 const app = express();
 
 const port = process.env.PORT || 3001;
 
+app.get("/", (req, res) => {
+  res.send("API Health: Healthy ✅");
+});
+
 const startServer = async () => {
   try {
-    // await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is running on port ${port}`));
   } catch (err) {
     console.error(err);
